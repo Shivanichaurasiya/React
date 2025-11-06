8951c018da47533bc9cd9a4e8ace56ebc446f7d3
Shivani Chaurasiya <shivanichaurasiya57132@gmail.com>
2025-11-06 10:47:15 +0530

    second commit

--- lazy-loading/src/App.jsx
+++ lazy-loading/src/App.jsx
@@ -1,17 +1,15 @@
 import React ,{lazy,Suspense} from "react";
-
-
 const About=lazy(()=>import('./components/About'))
-
+const Home=lazy(()=>import('./components/Home'))
 
 function App() {
-  
 
   return (
     <>
     <div className="bg-red-300">My name is shivani chaurasiya </div>
      <Suspense fallback={<div>Loading About page</div>}>
      <About/>
+     <Home/>
      </Suspense>
     </>
   )
